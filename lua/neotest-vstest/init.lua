a2d8ba7ae4cf96cc3a0cb999d12e287fef4ee9e2
Michael <michael.lyngs.social@mailbox.org>
2025-11-25 16:40:41 +0000

    fix(discovery): test if file contains test (#46)
    The method to determine if the file is a test file did not take into
    account that it actually had tests for the file. It just checked that
    the file is from a project with a client.
    There may be files that are not test files within the project.

--- lua/neotest-vstest/init.lua
+++ lua/neotest-vstest/init.lua
@@ -129,6 +129,18 @@ local function create_adapter(config)
       return false
     end
 
+    local tests_in_file = client:discover_tests_for_path(file_path)
+
+    if not tests_in_file or next(tests_in_file) == nil then
+      logger.debug(
+        string.format(
+          "neotest-vstest: marking file as non-test file since no tests was found in file %s",
+          file_path
+        )
+      )
+      return false
+    end
+
     return true
   end
 
